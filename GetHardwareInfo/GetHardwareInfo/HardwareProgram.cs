928610cadcb39c29a0c1baea1fcf5112348b1058
Avadhesh Kumar <is.kmr1394@gmail.com>
2020-02-17 00:06:37 +0530

    SystemInformationProgram

---
+++ GetHardwareInfo/GetHardwareInfo/HardwareProgram.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetHardwareInfo
+{
+    class HardwareProgram
+    {
+        public string Id { get; set; }
+        public string Manufacturer { get; set; }
+        public string SerialNumber { get; set; }
+        public string Product { get; set; }
+        public string Caption { get; set; }
+        public string SystemName { get; set; }
+    }
+
+    class SoftwarePrograme
+    {
+        public string DisplayName { get; set; }
+        public string Version { get; set; }
+        public string InstalledDate { get; set; }
+        public string Publisher { get; set; }
+        public string UnninstallCommand { get; set; }
+        public string ModifyPath { get; set; }
+
+    }
+}
---
+++ GetHardwareInfo/GetHardwareInfo/Program.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetHardwareInfo
+{
+    class Program
+    {
+
+        public void GetInfo()
+        {
+
+            List<SoftwarePrograme> softwareList = new List<SoftwarePrograme>();
+           string registry_key=  @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall";
+            using (Microsoft.Win32.RegistryKey key = Registry.LocalMachine.OpenSubKey(registry_key))
+            {
+                foreach (string subkey_name in key.GetSubKeyNames())
+                {
+                    using (RegistryKey subkey = key.OpenSubKey(subkey_name))
+                    {
+                        softwareList.Add(new SoftwarePrograme
+                        {
+                            DisplayName = (string)subkey.GetValue("DisplayName"),
+                            Version = (string)subkey.GetValue("DisplayVersion"),
+                            InstalledDate = (string)subkey.GetValue("InstallDate"),
+                            Publisher = (string)subkey.GetValue("Publisher"),
+                            UnninstallCommand = (string)subkey.GetValue("UninstallString"),
+                            ModifyPath = (string)subkey.GetValue("ModifyPath")
+                        });
+                        Console.WriteLine(subkey.GetValue("DisplayName"));
+                    }
+                }
+            }
+
+            string registrykey = @"HARDWARE\DEVICEMAP";
+            using (Microsoft.Win32.RegistryKey key = Registry.LocalMachine.OpenSubKey(registrykey))
+            {
+                foreach (string subkey_name in key.GetSubKeyNames())
+                {
+                    using (RegistryKey subkey = key.OpenSubKey(subkey_name))
+                    {
+                        Console.WriteLine(subkey.GetValue("DisplayName"));
+                    }
+                }
+            }
+
+            List<HardwareProgram> hardwareList = new List<HardwareProgram>();
+            ManagementClass mc = new ManagementClass("win32_processor");
+            ManagementObjectCollection moc = mc.GetInstances();
+            foreach (ManagementObject mo in moc)
+            {
+                hardwareList.Add(new HardwareProgram
+                {
+                    Id = Convert.ToString(mo.Properties["processorID"].Value),
+                    Manufacturer = Convert.ToString(mo.GetPropertyValue("Manufacturer")),
+                    SerialNumber = Convert.ToString(mo.GetPropertyValue("SerialNumber")),
+                    Caption=Convert.ToString(mo.GetPropertyValue("Caption")),
+                   SystemName= Convert.ToString(mo.GetPropertyValue("SystemName"))
+                });
+            }
+        }
+    }
+
+                        
+    class test { 
+
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            obj.GetInfo();
+
+            Console.ReadLine();
+        }
+    }
+}
